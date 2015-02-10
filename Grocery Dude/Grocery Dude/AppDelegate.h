a6246f9dc88a2878773d92791de964b16c380a38
Victor Chee <victor.qhj@gmail.com>
2015-02-10 22:17:01 +0800

    add initial project

---
+++ Grocery Dude/Grocery Dude/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grocery Dude
+//
+//  Created by Victor Chee on 15/2/10.
+//  Copyright (c) 2015年 Victor Chee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
