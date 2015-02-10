bf5aaa328ada3761dfd6309c43751d7a94c4cde6
Victor Chee <victor.qhj@gmail.com>
2015-02-10 23:09:29 +0800

    add Core Data Helper

--- Grocery Dude/Grocery Dude/AppDelegate.m
+++ Grocery Dude/Grocery Dude/AppDelegate.m
@@ -13,7 +13,20 @@
 @end
 
 @implementation AppDelegate
+#define debug 1
 
+- (CoreDataHelper *)cdh
+{
+    if (debug == 1) {
+        NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
+    }
+    
+    if (_coreDataHelper) {
+        _coreDataHelper = [[CoreDataHelper alloc] init];
+        [_coreDataHelper setupCoreData];
+    }
+    return _coreDataHelper;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -28,6 +41,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[self cdh] saveContext];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -40,6 +54,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[self cdh] saveContext];
 }
 
 @end
---
+++ Grocery Dude/Grocery Dude/CoreDataHelper.h
@@ -0,0 +1,22 @@
+//
+//  CoreDataHelper.h
+//  Grocery Dude
+//
+//  Created by Victor Chee on 15/2/10.
+//  Copyright (c) 2015年 Victor Chee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataHelper : NSObject
+
+@property (nonatomic, readonly) NSManagedObjectContext *context;
+@property (nonatomic, readonly) NSManagedObjectModel *model;
+@property (nonatomic, readonly) NSPersistentStoreCoordinator *coordinator;
+@property (nonatomic, readonly) NSPersistentStore *store;
+
+- (void)setupCoreData;
+- (void)saveContext;
+
+@end
